d12dd26fcc464a3792ecc1feb4cc01cd6ab5466e
Mark Lierman <marklierman@hotmail.com>
2023-05-29 11:26:09 -0400

    No Refunds: Added exceptions for HUB Parts and Power Shards

--- NoRefunds/Source/NoRefunds/Private/NoRefundsModule.cpp
+++ NoRefunds/Source/NoRefunds/Private/NoRefundsModule.cpp
@@ -1,20 +1,43 @@
 #include "NoRefundsModule.h"
 #include "Equipment/FGBuildGunDismantle.h"
 #include "Patching/NativeHookManager.h"
-#include "FGInventoryComponent.h"
 
+//#pragma optimize("", off)
 void FNoRefundsModule::StartupModule() {
 
-#if !WITH_EDITOR
 	TArray< FInventoryStack > refund = TArray< FInventoryStack >();
 	SUBSCRIBE_METHOD(UFGBuildGunStateDismantle::GetDismantleRefund, [=](auto& scope, const UFGBuildGunStateDismantle* self)
 		{
 			TArray< FInventoryStack > refund = scope(self);
-			refund.Empty();
+			refund = AllowDismantleRefund(refund);
 			scope.Override(refund);
 		});
+#if !WITH_EDITOR
 #endif
 }
 
+TArray<FInventoryStack> FNoRefundsModule::AllowDismantleRefund(TArray<FInventoryStack> refund)
+{
+	TArray<FInventoryStack> newRefund;
+	if (refund.Num() > 0)
+	{
+		for (auto stack : refund)
+		{
+			auto itemClass = stack.Item.GetItemClass()->GetName();
+			if (itemClass.Equals("Desc_HUBParts_C") || itemClass.Equals("Desc_CrystalShard_C"))
+			{
+				newRefund.Add(stack);
+			}
+		}
+	}
+	if (newRefund.Num() > 0)
+	{
+		return newRefund;
+	}
+	refund.Empty();
+	return refund;
+}
+
+//#pragma optimize("", on)
 
 IMPLEMENT_GAME_MODULE(FNoRefundsModule, NoRefunds);
--- NoRefunds/Source/NoRefunds/Public/NoRefundsModule.h
+++ NoRefunds/Source/NoRefunds/Public/NoRefundsModule.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "Modules/ModuleManager.h"
+#include "FGInventoryComponent.h"
 
 class FNoRefundsModule : public FDefaultGameModuleImpl {
 public:
 	virtual void StartupModule() override;
 
 	virtual bool IsGameModule() const override { return true; }
+
+	TArray<FInventoryStack> AllowDismantleRefund(TArray< FInventoryStack > refund);
 };
