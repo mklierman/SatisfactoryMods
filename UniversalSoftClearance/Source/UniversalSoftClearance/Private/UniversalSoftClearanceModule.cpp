96caabcf03a3a81ccd685fbb00e2ec65cc1ecb7f
Mark Lierman <marklierman@hotmail.com>
2023-11-20 15:37:58 -0500

    Added blueprint soft clearance

--- UniversalSoftClearance/Source/UniversalSoftClearance/Private/UniversalSoftClearanceModule.cpp
+++ UniversalSoftClearance/Source/UniversalSoftClearance/Private/UniversalSoftClearanceModule.cpp
@@ -1,20 +1,41 @@
 #include "UniversalSoftClearanceModule.h"
-#include "Hologram/FGFactoryBuildingHologram.h"
-#include "FGClearanceComponent.h"
 #include "Patching/NativeHookManager.h"
 
+//#pragma optimize("", off)
 void FUniversalSoftClearanceModule::StartupModule() {
 	AFGHologram* hCDO = GetMutableDefault<AFGHologram>();
 
 #if !WITH_EDITOR
-	SUBSCRIBE_METHOD_VIRTUAL(AFGHologram::SetupClearance, hCDO, [](auto scope, AFGHologram* self, class UFGClearanceComponent* clearanceComponent)
+	SUBSCRIBE_METHOD_VIRTUAL(AFGHologram::SetupClearance, hCDO, [=](auto scope, AFGHologram* self, class UFGClearanceComponent* clearanceComponent)
 		{
-			class UFGClearanceComponent* cc = clearanceComponent;
-			cc->mIsSoftClearance = true;
-			scope(self, cc);
+			SetupClearance(self, clearanceComponent);
+			scope(self, clearanceComponent);
+		});
+
+
+	SUBSCRIBE_METHOD_AFTER(AFGBlueprintHologram::GenerateCollisionObjects, [=](AFGBlueprintHologram* self, const TArray< AFGBuildable* >& buildables)
+		{
+			GenerateCollisionObjects(self, buildables);
 		});
 #endif
 }
+
+void FUniversalSoftClearanceModule::SetupClearance(AFGHologram* self, UFGClearanceComponent* clearanceComponent)
+{
+	auto newthing = self;
+	clearanceComponent->mIsSoftClearance = true;
+}
+void FUniversalSoftClearanceModule::GenerateCollisionObjects(AFGBlueprintHologram* self, const TArray<AFGBuildable*>& buildables)
+{
+	if (self && self->mClearanceComponents.Num() > 0)
+	{
+		for (auto cc : self->mClearanceComponents)
+		{
+			cc->mIsSoftClearance = true;
+		}
+	}
+}
+//#pragma optimize("", on)
 
 
 IMPLEMENT_GAME_MODULE(FUniversalSoftClearanceModule, UniversalSoftClearance);
--- UniversalSoftClearance/Source/UniversalSoftClearance/Public/UniversalSoftClearanceModule.h
+++ UniversalSoftClearance/Source/UniversalSoftClearance/Public/UniversalSoftClearanceModule.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include "Modules/ModuleManager.h"
+#include "Hologram/FGFactoryBuildingHologram.h"
+#include "Hologram/FGBlueprintHologram.h"
+#include "FGClearanceComponent.h"
 
 class FUniversalSoftClearanceModule : public FDefaultGameModuleImpl {
 public:
 	virtual void StartupModule() override;
 
 	virtual bool IsGameModule() const override { return true; }
+
+	void SetupClearance(AFGHologram* self, class UFGClearanceComponent* clearanceComponent);
+
+	void GenerateCollisionObjects(AFGBlueprintHologram* self, const TArray< AFGBuildable* >& buildables);
 };
