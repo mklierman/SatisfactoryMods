0608976171edeb66535d76ff70abba7c7da09a32
Bababooie (Kyle) <kyle_de_boer@hotmail.com>
2025-02-19 17:06:23 +1000

    removed redundant LB stuffs

--- LoadBalancers (2) Folie a Deux/Source/LoadBalancers/Private/LBModularLoadBalancer_ConnectorHologram.cpp
+++
@@ -1,7 +0,0 @@
-#include "LBModularLoadBalancer_ConnectorHologram.h"
-
-// Build step
-
-// Step 1 - placement
-// Step 2 - Stretch along axis player is looking in 1m increments until collision with line trace
-// Step 2.5 - If collided with snap point don't go past it
--- LoadBalancers (2) Folie a Deux/Source/LoadBalancers/Private/LoadBalancers_Subsystem_CPP.cpp
+++
@@ -1,6 +0,0 @@
-#include "LoadBalancers_Subsystem_CPP.h"
-
-bool ALoadBalancers_Subsystem_CPP::ShouldSave_Implementation() const
-{
-    return true;
-}
