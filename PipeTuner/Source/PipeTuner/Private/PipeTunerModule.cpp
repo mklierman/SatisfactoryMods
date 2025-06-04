e8734940cef1f6900bc312a701f65a69ec7605a8
Mark Lierman <marklierman@hotmail.com>
2025-06-04 12:38:42 -0400

    Constant Geothermal fix for Vanilla Upgrades

--- PipeTuner/Source/PipeTuner/Private/PipeTunerModule.cpp
+++ PipeTuner/Source/PipeTuner/Private/PipeTunerModule.cpp
@@ -7,69 +7,54 @@
 #include "FGGameMode.h"
 #include "Hologram/FGPipelinePumpHologram.h"
 #include "Hologram/FGPipelineAttachmentHologram.h"
-#include <Logging/StructuredLog.h>
-#include <FGPipeNetwork.h>
 
-DEFINE_LOG_CATEGORY(LogPipeTuner);
+void GameModePostLogin(CallScope<void(*)(AFGGameMode*, APlayerController*)>& scope, AFGGameMode* gm,
+	APlayerController* pc)
+{
 
-
+}
 
 void FPipeTunerModule::StartupModule() {
 
 #if !WITH_EDITOR
-	AFGPipeNetwork* pnCDO = GetMutableDefault<AFGPipeNetwork>();
-	SUBSCRIBE_METHOD_VIRTUAL_AFTER(AFGPipeNetwork::BeginPlay,pnCDO, [this](AFGPipeNetwork* self)
-		{
-			UE_LOGFMT(LogPipeTuner, Display, "mGravity: {0}", self->mGravity);
-			UE_LOGFMT(LogPipeTuner, Display, "mFluidFriction: {0}", self->mFluidFriction);
-			UE_LOGFMT(LogPipeTuner, Display, "mFluidDensity: {0}", self->mFluidDensity);
-			UE_LOGFMT(LogPipeTuner, Display, "mFluidViscosity: {0}", self->mFluidViscosity);
-		});
-
 	AFGBuildablePipeline* plCDO = GetMutableDefault<AFGBuildablePipeline>();
-	SUBSCRIBE_METHOD_VIRTUAL_AFTER(AFGBuildablePipeline::BeginPlay, plCDO, [this](AFGBuildablePipeline* self)
+	SUBSCRIBE_METHOD_VIRTUAL_AFTER(AFGBuildablePipeline::BeginPlay, plCDO, [=](AFGBuildablePipeline* self)
 		{
 
 			FPipeTuner_ConfigStruct ModConfig = FPipeTuner_ConfigStruct::GetActiveConfig(self->GetWorld());
 			float mk2mult = ModConfig.Mk2PipeVolumeMultiplier;
 			float mk1mult = ModConfig.Mk1PipeVolumeMultiplier;
-			UE_LOGFMT(LogPipeTuner, Display, "OVERFILL_USED_FOR_PRESSURE_PCT: {0}", FFluidBox::OVERFILL_USED_FOR_PRESSURE_PCT);
-			UE_LOGFMT(LogPipeTuner, Display, "PRESSURE_LOSS: {0}", FFluidBox::PRESSURE_LOSS);
-			//FFluidBox::OVERFILL_USED_FOR_PRESSURE_PCT = ModConfig.OverfillPressurePercent;
-			//FFluidBox::PRESSURE_LOSS = ModConfig.PressureLossPercent;
+			FFluidBox::OVERFILL_USED_FOR_PRESSURE_PCT = ModConfig.OverfillPressurePercent;
+			FFluidBox::PRESSURE_LOSS = ModConfig.PressureLossPercent;
 			//self->mFlowLimit = self->mFlowLimit / FluidAmount;
 			//self->mFluidBox.MaxContent = self->mFluidBox.MaxContent / FluidAmount;
-			//self->mFluidBox.MaxOverfillPct = ModConfig.OverfillPercent;
-			UE_LOGFMT(LogPipeTuner, Display, "mFluidBox.MaxOverfillPct: {0}", self->mFluidBox.MaxOverfillPct);
+			self->mFluidBox.MaxOverfillPct = ModConfig.OverfillPercent;
 
 			auto name = self->GetName();
 			if (name.Contains("MK2"))
 			{
-				UE_LOGFMT(LogPipeTuner, Display, "mFluidBoxMaxContent: {0}", self->mFluidBox.MaxContent);
-				//self->mFluidBox.MaxContent = self->mFluidBox.MaxContent * mk2mult;
+				self->mFluidBox.MaxContent = self->mFluidBox.MaxContent * mk2mult;
 			}
 			else
 			{
-				UE_LOGFMT(LogPipeTuner, Display, "mFluidBoxMaxContent: {0}", self->mFluidBox.MaxContent);
-				//self->mFluidBox.MaxContent = self->mFluidBox.MaxContent * mk1mult;
+				self->mFluidBox.MaxContent = self->mFluidBox.MaxContent * mk1mult;
 			}
 		});
 
+	AFGGameMode* LocalGameMode = GetMutableDefault<AFGGameMode>();
+	SUBSCRIBE_METHOD_VIRTUAL(AFGGameMode::PostLogin, LocalGameMode, &GameModePostLogin);
 
 
 	AFGPipelineAttachmentHologram* pah = GetMutableDefault<AFGPipelineAttachmentHologram>();
-	SUBSCRIBE_METHOD_VIRTUAL(AFGPipelineAttachmentHologram::BeginPlay, pah, [this](auto scope, AFGPipelineAttachmentHologram* self)
+	SUBSCRIBE_METHOD_VIRTUAL(AFGPipelineAttachmentHologram::BeginPlay, pah, [=](auto scope, AFGPipelineAttachmentHologram* self)
 		{
 			auto pumphg = Cast< AFGPipelinePumpHologram>(self);
 			if (pumphg)
 			{
-				UE_LOGFMT(LogPipeTuner, Display, "mMaxTraversalDistance: {0}", pumphg->mMaxTraversalDistance);
-				UE_LOGFMT(LogPipeTuner, Display, "mMaxJunctionRecursions: {0}", pumphg->mMaxJunctionRecursions);
-				UE_LOGFMT(LogPipeTuner, Display, "mOffsetEstimationBinaryDivisionCount: {0}", pumphg->mOffsetEstimationBinaryDivisionCount);
 				FPipeTuner_ConfigStruct ModConfig = FPipeTuner_ConfigStruct::GetActiveConfig(self->GetWorld());
-				//pumphg->mMaxTraversalDistance = ModConfig.PumpTraversalDistance * 100;
-				//pumphg->mMaxJunctionRecursions = ModConfig.PumpJunctionRecursions;
-				//pumphg->mOffsetEstimationBinaryDivisionCount = ModConfig.PumpOffsetEstimationCount;
+				pumphg->mMaxTraversalDistance = ModConfig.PumpTraversalDistance * 100;
+				pumphg->mMaxJunctionRecursions = ModConfig.PumpJunctionRecursions;
+				pumphg->mOffsetEstimationBinaryDivisionCount = ModConfig.PumpOffsetEstimationCount;
 				//if (ActiveHologramTimers.Find(pumphg) == nullptr)
 				//{
 				//	ActiveHologramTimers.Add(pumphg, PumpTimer);
