88d4d8c5d92bb0d8b57e706cfcdf814d9f7b1140
Mark Lierman <marklierman@hotmail.com>
2025-07-03 09:08:14 -0400

    UniversalVehiclePaths DS fix

--- UniversalVehiclePaths/Source/UniversalVehiclePaths/Private/UniversalVehiclePathsModule.cpp
+++ UniversalVehiclePaths/Source/UniversalVehiclePaths/Private/UniversalVehiclePathsModule.cpp
@@ -1,20 +1,35 @@
 #include "UniversalVehiclePathsModule.h"
 #include "Patching/NativeHookManager.h"
-#include "FGVehicleSubsystem.h"
-#include "WheeledVehicles/FGWheeledVehicle.h"
-#include "WheeledVehicles/FGWheeledVehicleInfo.h"
 
 DEFINE_LOG_CATEGORY(UniversalVehiclePaths_Log);
 
+#pragma optimize("", off)
 void FUniversalVehiclePathsModule::StartupModule() {
 #if !WITH_EDITOR
-	SUBSCRIBE_METHOD(AFGVehicleSubsystem::FindSavedWheeledVehiclePaths, [](auto& scope, AFGVehicleSubsystem* self, const FString& textFilter, TSubclassOf< class AFGWheeledVehicle > typeFilter, const AFGWheeledVehicleInfo* vehicle, TArray< AFGSavedWheeledVehiclePath* >& result)
+#endif
+	SUBSCRIBE_METHOD(AFGVehicleSubsystem::FindSavedWheeledVehiclePaths, [this](auto& scope, AFGVehicleSubsystem* self, const FString& textFilter, TSubclassOf< class AFGWheeledVehicle > typeFilter, const AFGWheeledVehicleInfo* vehicle, TArray< AFGSavedWheeledVehiclePath* >& result)
 		{
+			if (!self || !vehicle)
+			{
+				return;
+			}
 			//UE_LOG(UniversalVehiclePaths_Log, Display, TEXT("FindSavedWheeledVehiclePaths"));
+			if (!vehicle->GetVehicle() && vehicle->mReplicatedVehicle)
+			{
+
+				if (vehicle->mReplicatedVehicle->GetClass()->GetSuperClass()->GetName().Contains("6W"))
+				{
+					scope(self, textFilter, vehicle->GetVehicle()->GetClass()->GetSuperClass()->GetSuperClass(), vehicle, result);
+				}
+				else
+				{
+					scope(self, textFilter, vehicle->mReplicatedVehicle->GetClass()->GetSuperClass(), vehicle, result);
+				}
+				return;
+			}
 			if (vehicle->GetVehicle()->GetClass()->GetSuperClass()->GetName().Contains("6W"))
 			{
 				scope(self, textFilter, vehicle->GetVehicle()->GetClass()->GetSuperClass()->GetSuperClass(), vehicle, result);
-				return;
 			}
 			else
 			{
@@ -22,8 +37,14 @@ void FUniversalVehiclePathsModule::StartupModule() {
 			}
 
 		});
-#endif
 }
 
+void FUniversalVehiclePathsModule::FindSavedPaths(AFGVehicleSubsystem* self, const AFGWheeledVehicleInfo* vehicle)
+{
+	auto newSelf = self;
+	auto newVehicle = vehicle;
+}
+
+#pragma optimize("", on)
 
 IMPLEMENT_GAME_MODULE(FUniversalVehiclePathsModule, UniversalVehiclePaths);
--- UniversalVehiclePaths/Source/UniversalVehiclePaths/Public/UniversalVehiclePathsModule.h
+++ UniversalVehiclePaths/Source/UniversalVehiclePaths/Public/UniversalVehiclePathsModule.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Modules/ModuleManager.h"
+#include "FGVehicleSubsystem.h"
+#include "WheeledVehicles/FGWheeledVehicle.h"
+#include "WheeledVehicles/FGWheeledVehicleInfo.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(UniversalVehiclePaths_Log, Display, All);
 class FUniversalVehiclePathsModule : public FDefaultGameModuleImpl {
@@ -8,4 +11,6 @@ public:
 	virtual void StartupModule() override;
 
 	virtual bool IsGameModule() const override { return true; }
+
+	void FindSavedPaths(AFGVehicleSubsystem* self, const AFGWheeledVehicleInfo* vehicle);
 };
