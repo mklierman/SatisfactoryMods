da2492a8b088b2f6290bdd439ac24b9b056bce27
Mark Lierman <marklierman@hotmail.com>
2025-11-06 11:04:06 -0500

    Underwater Construction

---
+++ UnderwaterConstruction/Source/UnderwaterConstruction/Private/UnderwaterConstruction.cpp
@@ -0,0 +1,35 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnderwaterConstruction.h"
+#include "FGCharacterPlayer.h"
+#include "Patching/NativeHookManager.h"
+
+#define LOCTEXT_NAMESPACE "FUnderwaterConstructionModule"
+
+void FUnderwaterConstructionModule::StartupModule()
+{
+#if !WITH_EDITOR
+	SUBSCRIBE_METHOD(AFGCharacterPlayer::CanEquipBuildGun, [this](auto& scope, const AFGCharacterPlayer* self)
+		{
+			scope.Override(true);
+		});
+	SUBSCRIBE_METHOD(AFGCharacterPlayer::CanEquipBuildGunForDismantle, [this](auto& scope, const AFGCharacterPlayer* self)
+		{
+			scope.Override(true);
+		});
+	SUBSCRIBE_METHOD(AFGCharacterPlayer::CanEquipBuildGunForPaint, [this](auto& scope, const AFGCharacterPlayer* self)
+		{
+			scope.Override(true);
+		});
+#endif
+}
+
+void FUnderwaterConstructionModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUnderwaterConstructionModule, UnderwaterConstruction)
