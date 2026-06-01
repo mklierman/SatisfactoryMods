189dcff795134ef7bcf3c70a62bb9ad2bec2f1fa
Mark Lierman <marklierman@hotmail.com>
2026-06-01 08:19:50 -0400

    Vanilla Upgrades attempted power storage fix

--- VanillaUpgrades/Source/VanillaUpgrades/Private/VU_BPFL.cpp
+++ VanillaUpgrades/Source/VanillaUpgrades/Private/VU_BPFL.cpp
@@ -11,13 +11,24 @@ void UVU_BPFL::PowerStorageBeginPlay(AFGBuildablePowerStorage* powerStorage)
 
 void UVU_BPFL::PowerStorageRestart(AFGBuildablePowerStorage* powerStorage)
 {
-	powerStorage->mPowerInfo->InitializeBatteryInfo(powerStorage->mPowerStoreCapacity, powerStorage->mPowerInputCapacity);
-	powerStorage->mBatteryInfo = powerStorage->mPowerInfo->GetBatteryInfo();
-	float clampedPowerStore = FMath::Min(powerStorage->mPowerStoreCapacity, powerStorage->mPowerStore);
-	powerStorage->mPowerStore = clampedPowerStore;
-	powerStorage->mBatteryInfo->SetPowerStore(clampedPowerStore);
-	powerStorage->mBatteryInfo->SetActive(true);
-	powerStorage->UpdatePropertiesOnGameThread(EBatteryStatus::BS_Idle, powerStorage->CalculateIndicatorLevel());
+	if (powerStorage)
+	{
+		auto powerInfo = powerStorage->mPowerInfo;
+		if (powerInfo)
+		{
+			powerInfo->InitializeBatteryInfo(powerStorage->mPowerStoreCapacity, powerStorage->mPowerInputCapacity);
+			auto batteryInfo = powerStorage->mBatteryInfo;
+			if (batteryInfo)
+			{
+				batteryInfo = powerInfo->GetBatteryInfo();
+				float clampedPowerStore = FMath::Min(powerStorage->mPowerStoreCapacity, powerStorage->mPowerStore);
+				powerStorage->mPowerStore = clampedPowerStore;
+				batteryInfo->SetPowerStore(clampedPowerStore);
+				batteryInfo->SetActive(true);
+				powerStorage->UpdatePropertiesOnGameThread(EBatteryStatus::BS_Idle, powerStorage->CalculateIndicatorLevel());
+			}
+		}
+	}
 }
 
 void UVU_BPFL::CalculateProductionCycleTime(AFGBuildableResourceExtractor* extractor)
